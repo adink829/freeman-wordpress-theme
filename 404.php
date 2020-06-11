c30dd6dfe89ed685dbb3b22a81996e4ebee4660b
adink829 <alicia.dinkel@gmail.com>
2020-06-10 22:44:26 -0400

    landscape menu and 404

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?> 
+    <div class="category-body">
+    <div class="header-background"> </div>
+            <main class="page-content">
+                    <h2 class="page-header">
+                    404
+                    </h2>
+                    <h3 class="page-subheader">
+                    Whoopsie daisy!
+                    </h3>
+                    <div class="page-description">
+                    You're in the wrong place, silly goose
+                    </div>
+            </main>
+    </div>
+
+</body>
+</html>
--- page.php
+++ page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?> 
     <div class="category-body">
+    <div class="header-background"> </div>
         <?php if ( have_posts() ) : ?>
             <main class="page-content">
                 <?php while ( have_posts() ) : the_post(); ?>
